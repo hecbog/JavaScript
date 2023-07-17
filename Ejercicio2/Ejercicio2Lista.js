2a0fa2e5073b0883df5fe97532fb9b6618c024a8
hecbog <123418282+hecbog@users.noreply.github.com>
2023-07-16 22:02:05 -0400

    Add files via upload

---
+++ Ejercicio2/Ejercicio2Lista.js
@@ -0,0 +1,15 @@
+
+const nombre = "Hector";
+const edad = 39;
+const desarrollador = true;
+const nacimiento = new Date("may 3 1984");
+const libroFav = {
+    titulo: "Como ganar amigos",
+    autor: "Dale Carnegie",
+    fecha: "1 oct 2008",
+    url: "https://www.amazon.com/-/es/Dale-Carnegie/dp/8493664928"
+}
+
+const lista = [nombre, edad, desarrollador, nacimiento, libroFav];
+
+console.log(lista);
