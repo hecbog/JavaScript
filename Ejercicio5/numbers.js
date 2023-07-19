0685e30b6b917ce4a3143f8dd85972746106f292
Hector Bogarin <hectorbogcar@gmail.com>
2023-07-18 21:12:32 -0400

    Ejercicio sobre numeros

---
+++ Ejercicio5/numbers.js
@@ -0,0 +1,9 @@
+const intAltura = 174;
+const fAltura = 1.74;
+const fPeso = 68.5;
+
+const redonAltura = Math.ceil(fAltura);
+const redonPeso = Math.floor(fPeso);
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
