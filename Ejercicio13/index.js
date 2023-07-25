4fca38763d5658b8f816b1cf52112f997f74746a
Hector Bogarin <hectorbogcar@gmail.com>
2023-07-24 21:17:31 -0400

    Ejercicio13 sobre modulos en JavaScript

---
+++ Ejercicio13/index.js
@@ -0,0 +1,7 @@
+import { suma, multiplica  } from "./controller.js";
+
+import chalk from 'chalk';
+
+
+console.log(multiplica(1, 2), suma(4, 5))
+console.log(chalk.green(multiplica(1, 2), suma(4, 5)));
